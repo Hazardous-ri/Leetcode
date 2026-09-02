0b170b36259ae874a3d391cd34d6f38fa4241ea1
Rishabh mishra <rishabhmi309@gmail.com>
2026-09-02 12:30:36 +0530

    Time: 0 ms (100%), Space: 30.4 MB (7.23%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+       
+          // mai khud se comment likh rha chaap nhi rha bsdk 
+          //so if whole array is even it is always be gonna true  due to even parity 
+          // if there is a single odd number in the array then by keeping the single odd number we can make this whole arrya odd 
+          // now if there are more than 2 odd number or 2 odd number we can make awhole array even so in every case it is gonna be true  
+        return true ;
+    }
+};
