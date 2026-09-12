f1eec342087c7e15e857ba0c656d98591c3e68b6
Rishabh mishra <rishabhmi309@gmail.com>
2026-09-12 16:49:02 +0530

    Time: 1352 ms (26.08%), Space: 454.3 MB (5.43%) - LeetHub

---
+++ 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.cpp
@@ -0,0 +1,81 @@
+class Solution {
+public:
+    vector<int> maximumWeight(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+
+        // Store: {left, right, weight, original_index}
+        vector<array<long long, 4>> a;
+
+        for (int i = 0; i < n; i++) {
+            a.push_back({
+                intervals[i][0],
+                intervals[i][1],
+                intervals[i][2],
+                i
+            });
+        }
+
+        // Sort by starting point
+        sort(a.begin(), a.end());
+
+        // next[i] = first interval whose left > a[i].right
+        vector<int> next(n);
+
+        for (int i = 0; i < n; i++) {
+            int l = i + 1;
+            int r = n;
+
+            while (l < r) {
+                int mid = l + (r - l) / 2;
+
+                if (a[mid][0] > a[i][1])
+                    r = mid;
+                else
+                    l = mid + 1;
+            }
+
+            next[i] = l;
+        }
+
+        // dp[i][k] = best result from i onward using at most k intervals
+        struct State {
+            long long score;
+            vector<int> indices;
+        };
+
+        vector<vector<State>> dp(n + 1, vector<State>(5));
+
+        // Compare two states
+        auto better = [&](State A, State B) {
+            if (A.score != B.score)
+                return A.score > B.score;
+
+            return A.indices < B.indices;
+        };
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            for (int k = 1; k <= 4; k++) {
+
+                // Option 1: don't take current interval
+                State skip = dp[i + 1][k];
+
+                // Option 2: take current interval
+                State take = dp[next[i]][k - 1];
+
+                take.score += a[i][2];
+                take.indices.push_back(a[i][3]);
+
+                // We need indices in increasing order
+                sort(take.indices.begin(), take.indices.end());
+
+                if (better(take, skip))
+                    dp[i][k] = take;
+                else
+                    dp[i][k] = skip;
+            }
+        }
+
+        return dp[0][4].indices;
+    }
+};
